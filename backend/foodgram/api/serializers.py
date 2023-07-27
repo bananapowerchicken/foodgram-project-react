7f7a3468d2d175a7065100826208726b400de6e4
Анна МительковаÐ <221882@mail.ru>
2023-07-27 17:27:12 +0300

    ingredient in recipe is shown correctly like in aoi docs

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -2,7 +2,7 @@ from users.models import CustomUser
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework.fields import SerializerMethodField
 from rest_framework import serializers
-from recipes.models import Tag, Recipe
+from recipes.models import Tag, Recipe, IngredientInRecipe
 
 # сериалайзеры отвечают, например, за выводлимый вид в postman рез-та запроса
 
@@ -38,8 +38,21 @@ class TagSerializer(serializers.ModelSerializer):
 # автор дб объект user
 # тег - список tag objects
 
+class IngredientInRecipeSerializer(serializers.ModelSerializer):
+    # определяем поля из модели ингредиента ручками
+    name = serializers.CharField(source='ingredient.name')
+    measurement_unit = serializers.CharField(source='ingredient.measurement_unit')
+
+    class Meta:
+        model = IngredientInRecipe
+        fields = ('id', 'name', 'measurement_unit', 'amount')
+
 class RecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True)  # это рабочий фикс  вывода тегов
+    # замена истояника получения данных, был - модкль рецепта, стал - ингр в рецепте
+    # чтобы получить ингредиент - используй атрибут в source от recipe
+    # вебинар 1:15:25
+    ingredient = IngredientInRecipeSerializer(many=True, source='ingredientinrecipe_set')
     # author = CustomUserSerializer(many=True)
     class Meta:
         model = Recipe
